216c7143a8c916e4eea58b851ec9d89daa6732f1
JWC1226 <peter72769@gmail.com>
2026-07-01 15:37:53 +0900

    Delete 01_MiniProject/src/com/project directory

--- 01_MiniProject/src/com/project/controller/FileManageable.java
+++
@@ -1,9 +0,0 @@
-package com.project.controller;
-
-import com.project.model.User;
-
-public interface FileManageable {
-//	파일 입출력
-	public void loadFromFile();
-	public void saveUserToFile(User user);
-}
